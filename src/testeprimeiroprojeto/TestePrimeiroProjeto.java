3e04d84f5575303706a21d0b1ef09b1b224038fb
leonardo_delfino <leonardo_delfino@10.1.48.160>
2014-05-27 14:23:05 -0300

    Inicialização de um projeto.

---
+++ src/testeprimeiroprojeto/TestePrimeiroProjeto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testeprimeiroprojeto;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author leonardo_delfino
+ */
+public class TestePrimeiroProjeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "testando");
+    }
+}
